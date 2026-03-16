12a02494485619260facc7fa2b172531831b8177
강민주 (Minju) <ju2d68@g.skku.edu>
2026-03-16 14:29:55 +0900

    Create 주식가격.py

---
+++ Programmers/01_Stack_Queue/주식가격.py
@@ -0,0 +1,13 @@
+# 
+def solution(prices):
+    answer = [0]* len(prices)
+    stack = []
+    for i in range(len(prices)):
+        while stack and prices[i] < prices[stack[-1]]:
+            past_time = stack.pop()
+            answer[past_time] = i-past_time
+        stack.append(i)
+    while stack:
+        past_time = stack.pop()
+        answer[past_time] = len(prices)-1-past_time
+    return answer

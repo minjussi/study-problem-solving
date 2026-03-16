5fc32d110954126098783b5e11a0e8c15a4c8646
강민주 (Minju) <ju2d68@g.skku.edu>
2026-03-16 14:30:58 +0900

    Create 기능개발.py

---
+++ Programmers/01_Stack_Queue/기능개발.py
@@ -0,0 +1,21 @@
+# 음수를 나누는 것 (올림 가능)
+
+def solution(progresses, speeds):
+    answer = []
+    days = []
+    
+    for i in range(len(progresses)):
+        days.append(-(-(100-progresses[i]) // speeds[i]))
+    
+    count = 1
+    max_day = days[0]
+    
+    for i in range(1, len(days)):
+        if days[i] <= max_day:
+            count +=1
+        else:
+            answer.append(count)
+            count = 1
+            max_day = days[i]
+    answer.append(count)
+    return answer

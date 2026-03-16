6595322ab8809d57630150e798f98d376db7044e
강민주 (Minju) <ju2d68@g.skku.edu>
2026-03-16 14:19:09 +0900

    Create 다리를_지나는_트럭.py

---
+++ Programmers/다리를_지나는_트럭.py
@@ -0,0 +1,23 @@
+# 문제 해결 포인트: 0으로 채워서 시간의 흐름 표현하기
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    time = 0
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+    current_weight = 0 
+    
+    while bridge:
+        time += 1
+        passed_truck = bridge.popleft()
+        current_weight -= passed_truck
+        
+        if truck_weights:
+            if current_weight + truck_weights[0] <= weight:
+                next_truck = truck_weights.popleft()
+                bridge.append(next_truck)
+                current_weight += next_truck
+            else:
+                bridge.append(0)
+
+    return time

1587ae150d259fffdba017692581f8df0d140e08
Minju Kang <ju2d68@g.skku.edu>
2026-03-24 13:22:25 +0900

    Update 중요한단어를스포방지.py

--- Programmers/08_2025_카카오_하반기_1차/중요한단어를스포방지.py
+++ Programmers/08_2025_카카오_하반기_1차/중요한단어를스포방지.py
@@ -32,3 +32,41 @@ def solution(message, spoiler_ranges):
             if string not in important:
                 important.append(string)
     return len(important)
+
+# 더 나은 코드
+# set의 사용
+# 겹치는 문자가 나오면 반복문 break
+
+def solution(message, spoiler_ranges):
+    message_index = []
+    start = 0
+    for i in range(len(message)):
+        if message[i] == " ":
+            message_index.append([start, i-1])
+            start = i+1
+        if i == len(message)-1:
+            message_index.append([start, i])   
+            
+    # 1. 속도를 위해 리스트 대신 집합(set) 사용
+    not_important = set() 
+    candidate = []
+    
+    for index in message_index:
+        is_spoiler = False
+        start = index[0]
+        end = index[1]
+        for spoiler in spoiler_ranges:
+            if start <= spoiler[1] and spoiler[0] <= end:
+                candidate.append(message[start:end+1])
+                is_spoiler = True
+                break  # 2. 한 번이라도 겹치면 더 검사할 필요 없이 반복문 탈출!
+                
+        if not is_spoiler:
+            not_important.add(message[start:end+1]) # 리스트의 append 대신 set의 add 사용
+            
+    important = set() # 여기서도 속도를 위해 집합 사용
+    for string in candidate:
+        if string not in not_important:
+            important.add(string)
+            
+    return len(important)

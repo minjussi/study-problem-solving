5239dbe09595444fb8f8e4da5fb8eefa97eaa4ac
Minju Kang <ju2d68@g.skku.edu>
2026-03-27 14:10:57 +0900

    Update 다리를_지나는_트럭.py

--- Programmers/01_Stack_Queue/다리를_지나는_트럭.py
+++ Programmers/01_Stack_Queue/다리를_지나는_트럭.py
@@ -7,18 +7,18 @@ def solution(bridge_length, weight, truck_weights):
     time = 0
     bridge = deque([0] * bridge_length)  # 리스트를 넣어주기 [0, 0, 0] 이런 식으로
     truck_weights = deque(truck_weights) # 리스트 자체를 삽입 (반복문 사용 가능)
-    current_weight = 0 
+    current_weight = 0 # 이걸 기준으로 빼고 더하고를 계속 해서 weight와 비교를 해야함
     
     while bridge:
         time += 1
         passed_truck = bridge.popleft()
-        current_weight -= passed_truck
+        current_weight -= passed_truck # 지나간 건 빼고 !
         
         if truck_weights:
-            if current_weight + truck_weights[0] <= weight:
+            if current_weight + truck_weights[0] <= weight: # 비교 로직
                 next_truck = truck_weights.popleft()
                 bridge.append(next_truck)
-                current_weight += next_truck
+                current_weight += next_truck # 무게를 더해 주어야 함 !!
             else:
                 bridge.append(0)
 

25198f49e366f86d1443742ef554f30c0dda3cd1
Minju Kang <ju2d68@g.skku.edu>
2026-04-02 13:54:56 +0900

    Create 대장균크기에따라분류.sql

---
+++ Programmers/08_SQL/대장균크기에따라분류.sql
@@ -0,0 +1,18 @@
+SELECT id, CASE NTILE(4) OVER (ORDER BY size_of_colony desc) 
+  WHEN 1 THEN 'CRITICAL' WHEN 2 THEN 'HIGH' WHEN 3 THEN 'MEDIUM' ELSE 'LOW' 
+  END AS colony_name
+FROM ecoli_data
+ORDER BY id asc;
+
+--다른 풀이
+SELECT id, CASE
+  WHEN quart = 1 THEN 'CRITICAL'
+  WHEN quart = 2 THEN 'HIGH'
+  WHEN quart = 3 THEN 'MEDIUM'
+  ELSE 'LOW'
+  END AS colony_name
+FROM (
+  SELECT id, NTILE(4) OVER (ORDER BY size_of_colony desc) AS quart
+  FROM ecoli_data
+) AS graded
+ORDER BY id asc;

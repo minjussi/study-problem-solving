cb30e10767932525ef9f335aa4c49f989eadb3ac
Minju Kang <ju2d68@g.skku.edu>
2026-04-02 14:11:55 +0900

    Update 총거래금액조회.sql

--- Programmers/08_SQL/총거래금액조회.sql
+++ Programmers/08_SQL/총거래금액조회.sql
@@ -1,5 +1,5 @@
---순서: from->where->group by->having->select->order by
---select 절에서만 alias 가능
+-- 순서: from->where->group by->having->select->order by
+-- select 절에서만 alias 가능
 
 SELECT users.user_id, users.nickname, SUM(price) as total_sales
 FROM used_goods_board AS board join used_goods_user AS users on board.writer_id = users.user_id

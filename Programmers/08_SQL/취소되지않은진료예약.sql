8ffe52d4a7d837a8e29ef3ad0b342102fc318e0a
Minju Kang <ju2d68@g.skku.edu>
2026-04-02 13:35:05 +0900

    Update 취소되지않은진료예약.sql

--- Programmers/08_SQL/취소되지않은진료예약.sql
+++ Programmers/08_SQL/취소되지않은진료예약.sql
@@ -1,5 +1,7 @@
 --이중으로 join을 써야하기 때문에 from 절에서 가상 테이블 먼저 만들고, 다시 조회
 --뼈대 먼저 잡고, 어떻게 진행해나가야할지 판단한 다음, 코드 작성하기 (어떤 걸 출력해야하는지 명확히 파악 -> join을 효과적으로 해야 실수할 확률 떨어짐)
+--일반 컬럼은 as 안 해도 자동으로 apnt_ymd처럼 세팅 됨
+--그러나!! sum(), count() 같은 것들은 as로 지정 필수!!
 
 SELECT apnt.apnt_no, apnt.pt_name, apnt.pt_no, doctor.mcdp_cd, doctor.dr_name, apnt.apnt_ymd
 FROM (

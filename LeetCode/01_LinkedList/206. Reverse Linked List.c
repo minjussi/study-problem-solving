094a888137ce24f6703d89afe3b848e5b0dd3f6b
Minju Kang <ju2d68@g.skku.edu>
2026-08-27 15:39:47 +0900

    Update 206. Reverse Linked List.c

--- LeetCode/01_LinkedList/206. Reverse Linked List.c
+++ LeetCode/01_LinkedList/206. Reverse Linked List.c
@@ -5,6 +5,26 @@
  *     struct ListNode *next;
  * };
  */
+// head: memory address that points first node
+// node size: int val (4 byte)+ ListNode *next (8 byte)
 struct ListNode* reverseList(struct ListNode* head) {
+    // prev: start with NULL , previous node address
+    // curr: start with head, current node address
+    // next_curr: store next node temporarily
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    struct ListNode* next_curr = NULL;
+
+    // if curr is NULL -> there's no more node
+    while (curr != NULL) {
+        next_curr = curr->next;
+        // current node points previous node (change direction)
+        curr->next = prev;
+        prev = curr;
+        curr = next_curr;
+    }
     
+    // after while loop prev becomes new head
+    // leet code runs main code automatically
+    return prev;
 }
